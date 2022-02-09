26fa76d6c7d034f6c5938d67933af94b93453ceb
sunmeng <18742052343@163.com>
2022-02-09 12:30:19 +0800

    整个登录功能：生成验证码，登录推出功能，显示登录信息，账号设置，检查登录状态

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -27,4 +27,14 @@ public interface CommunityConstant {
      */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
+    /**
+     * 默认状态的登录凭证的超时时间
+     */
+    int DEFAULT_EXPIRID_SECONDS = 3600 * 12;
+
+    /**
+     * 记住状态下的登录凭证超时时间
+     */
+    int REMEMBER_EXPIRID_SECONDS = 3600 * 12;
+
 }
